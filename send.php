bce9984fef179b9a2d6e121a02fa5e5ced0810e1
Diego Borja <daborja1@espe.edu.ec>
2018-07-14 15:31:10 -0500

    email function

---
+++ send.php
@@ -0,0 +1,6 @@
+<?php
+    $destination="diego_anddy95@hotmail.com";
+    $latitude=$_POST["latitude"];
+    $longitude=$_POST["longitude"];
+    $menssage="Latitud: "+$latitude+"\nLongitud:"+$longitude;
+?>
