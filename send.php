3cc734b991efedc0898d0c0746b4629aef082531
Diego Borja <daborja1@espe.edu.ec>
2018-07-19 01:50:04 -0500

    only location

--- send.php
+++ send.php
@@ -1,6 +1,9 @@
 <?php
+    $document=new DomDocument;
+    $document->loadHTMLfile('index.html');
     $destination="diego_anddy95@hotmail.com";
-    $latitude=$_POST["latitude"];
-    $longitude=$_POST["longitude"];
+    $latitude=$document->getElementByID('latitude');
+    $longitude=$document->getElementByID('longitude');
     $menssage="Latitud: "+$latitude+"\nLongitud:"+$longitude;
+    mail($destination,"Coordenadas",$menssage);    
 ?>
